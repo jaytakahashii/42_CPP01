7b67d76d6df945ebcb8b42be8afe924805488f1e
Jay Takahashi <qsakura14555@gmail.com>
2025-03-17 20:50:19 +0900

    feat(SRC): ✨  Normal behavier is OK

---
+++ ex05/src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#include "Harl.hpp"
+
+int main() {
+  Harl harl;
+
+  std::cout << "Testing each log level:" << std::endl;
+  harl.complain("DEBUG");
+  harl.complain("INFO");
+  harl.complain("WARNING");
+  harl.complain("ERROR");
+
+  std::cout << "\nTesting unknown log level:" << std::endl;
+  harl.complain("UNKNOWN");
+
+  return 0;
+}
