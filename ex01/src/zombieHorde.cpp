042c100a24454a7e94fc97eb1135548a194f60f8
jtakahas <jtakahas@student.42tokyo.jp>
2025-04-01 18:29:28 +0900

    fix(SRC): 👾  if N is more 10

--- ex01/src/zombieHorde.cpp
+++ ex01/src/zombieHorde.cpp
@@ -2,6 +2,12 @@
 
 #include "Zombie.hpp"
 
+static std::string ft_to_string(int n) {
+  std::ostringstream oss;
+  oss << n;
+  return oss.str();
+}
+
 Zombie *zombieHorde(int N, std::string name) {
   if (N <= 0)
     // nullptr is unavailable in C++98, use NULL instead
@@ -10,7 +16,7 @@ Zombie *zombieHorde(int N, std::string name) {
   Zombie *horde = new Zombie[N];  // Allocate N Zombies on the heap
 
   for (int i = 0; i < N; i++) {
-    std::string strI = std::string(1, '1' + i);
+    std::string strI = ft_to_string(i + 1);
     horde[i].setName(name + strI);
   }
 
